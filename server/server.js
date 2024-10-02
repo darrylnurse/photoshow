42f387948e7c340ce2a1d959c835bfb03cd8cf9f
darcierre <darrnurse@gmail.com>
2024-10-02 16:10:43 -0400

    More changes for deply.

--- server/server.js
+++ server/server.js
@@ -20,8 +20,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/photos', photoRouter);
 app.use('/about', aboutRouter);
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 const PORT = process.env.PORT || 3001;
