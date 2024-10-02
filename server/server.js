dd5f348418ac250c34d8af270f5e558a8d42eb6d
darcierre <darrnurse@gmail.com>
2024-10-02 16:18:28 -0400

    Further changes for deploy.

--- server/server.js
+++ server/server.js
@@ -24,10 +24,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', '404.html'));
-});
-
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
