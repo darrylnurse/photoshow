ebef35b426866c354d2502d3fe89c8d808370cbd
darcierre <darrnurse@gmail.com>
2024-10-02 21:43:06 -0400

    Please be FINAL changes for deploy.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '/',
+  root: './public',
   base: '/',
   build: {
     outDir: '../server/public',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './public/index.html',     
+        photos: './public/photos.html',   
+        about: './public/about.html',     
+        notFound: './public/404.html' 
+      },
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]', 
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   },
   server: {
     proxy: {
